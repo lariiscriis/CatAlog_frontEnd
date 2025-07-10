79607ea8b01755d473ec9ede794a7fc82ce1ae27
milene Santos <milenesantosalmeida13@gmail.com>
2025-07-10 20:07:48 -0300

    arruma anotacoes

--- src/app/services/anotacao.service.ts
+++ src/app/services/anotacao.service.ts
@@ -10,24 +10,46 @@ export class AnotacaoService {
   private apiUrl = 'http://localhost:8080/anotacoes';
 
   constructor(private http: HttpClient) {}
-
+  private getToken(): string {
+    return localStorage.getItem('auth-token') || '';
+  }
   listarPorUsuario(idUsuario: string): Observable<Anotacao[]> {
-  return this.http.get<Anotacao[]>(`${this.apiUrl}/${idUsuario}`);
+  return this.http.get<Anotacao[]>(`${this.apiUrl}/${idUsuario}`, {
+    headers: {
+      Authorization: `Bearer ${this.getToken()}`
+    }
+  });
 }
 
   listarPorLivro(idUsuario: string, idLivro: string): Observable<Anotacao[]> {
-    return this.http.get<Anotacao[]>(`${this.apiUrl}/${idUsuario}/livro/${idLivro}`);
+    return this.http.get<Anotacao[]>(`${this.apiUrl}/${idUsuario}/livro/${idLivro}`, {
+    headers: {
+      Authorization: `Bearer ${this.getToken()}`
+    }
+  });
   }
 
   criar(anotacao: Anotacao): Observable<Anotacao> {
-    return this.http.post<Anotacao>(this.apiUrl, anotacao);
+    return this.http.post<Anotacao>(this.apiUrl, anotacao, {
+    headers: {
+      Authorization: `Bearer ${this.getToken()}`
+    }
+  });
   }
 
   atualizar(id: number, anotacao: Anotacao): Observable<Anotacao> {
-    return this.http.put<Anotacao>(`${this.apiUrl}/${id}`, anotacao);
+    return this.http.put<Anotacao>(`${this.apiUrl}/${id}`, anotacao, {
+    headers: {
+      Authorization: `Bearer ${this.getToken()}`
+    }
+  });
   }
 
   deletar(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, {
+    headers: {
+      Authorization: `Bearer ${this.getToken()}`
+    }
+  });
   }
 }
